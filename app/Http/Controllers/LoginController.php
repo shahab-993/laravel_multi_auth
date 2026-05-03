bc674974b44ff88c2f16b63451a389c5b3b8a50b
shahab-993 <ahmad.k.shahab@gmail.com>
2026-05-03 20:50:53 +0430

    laravel_multi_auth

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+
+class LoginController extends Controller
+{
+    public function index(){
+        return view('login');
+    }
+
+
+public function authenticate(Request $request){
+    $validator = Validator::make($request->all(),[
+        'email'=>'required|email',
+        'password'=>'required'
+    ]);
+    if($validator->passes()){
+        if(Auth::attemptWhen(['email'=> $request->email, 'password'=>$request->password])){
+
+        return redirect()->route('account.dashboard');
+
+        }else {
+            return redirect()->route('account.login')->with('error','Either email or password is incorrect.');
+        }
+
+    }else{
+        return redirect()->route('account.login')
+        ->withInput()
+        ->withErrors($validator);
+    }
+
+}
+
+public function register(){
+     return view('register');
+    }
+    public function processRegister(Request $request){
+      $validator = Validator::make($request->all(),[
+        'email'=>'required|email|unique:users',
+        'password'=>'required|confirmed'
+    ]);
+    if($validator->passes()){
+        $user =new User();
+        $user ->name = $request->name;
+        $user->email=$request->email;
+        $user-> password=Hash::make($request->password);
+        $user->role='customer';
+        $user->save();
+        return redirect()->route('account.login')->with('success',' You have registed successfully' );
+
+
+    }else{
+        return redirect()->route('account.register')
+        ->withInput()
+        ->withErrors($validator);
+  }
+}
+public function logout(){
+    Auth::logout();
+    return redirect()->route('account.login');
+}
+
+}
---
+++ bootstrap/app.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+     
+        $middleware->alias([
+    'admin.guest' => \App\Http\Middleware\AdminRedirect::class,    // که لاګ ان وي، ریډایرکټ کړي
+    'admin.auth'  => \App\Http\Middleware\AdminAuthenticate::class, // که لاګ ان نه وي، ریډایرکټ کړي
+]);
+
+        $middleware->redirectTo(
+            guests:'/account/login',
+            users:'/account/dashboard'
+        );
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
